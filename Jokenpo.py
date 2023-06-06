0ca5bc57da49b798df9260c26f53d6dcf7eb8da1
dariodsc <46139961+dariodsc@users.noreply.github.com>
2023-06-06 13:48:14 -0400

    Update Jokenpo.py
    - Ajuste na validação da variável JOGADOR
    - Add def saudação (função)
    - Update na def again (simplificada)

--- Jokenpo.py
+++ Jokenpo.py
@@ -1,19 +1,30 @@
-# Jogo de Jokenpo - PEDRA PAPEL TESOURA #
+# JOGO DE JOKENPO - FAMOSO PEDRA, PAPEL, TESOURA
 
-from random import randint    # para escolher um int aleatório dentro da lista #
+from random import randint
 
 def jokenpo():
+    saudacao()
     itens = ('Pedra', 'Papel', 'Tesoura')
-    computador = randint(0, 2)              #
-    print('Vamos jogar Jokenpo?! Vamos!')
-    print('''Escolha uma opção:\n
+    computador = randint(0, 2)
+    print('''
+    JOKENPO TIME!
+    Escolha uma opção:
     [ 0 ] - Pedra
     [ 1 ] - Papel 
     [ 2 ] - Tesoura\n''')
-    jogador = int(input('Qual a sua jogada? '))
-    print('*' * 30)
+    
+    while True:     # Válida a entrada do usuário
+        try:        
+            jogador = int(input('Qual a sua jogada? '))
+            if jogador < 0 or jogador > 2:
+                raise ValueError
+            break
+        except ValueError:
+            print('Jogada inválida. Digite um número entre 0 e 2.')
+            
     print('O Computador escolheu {}'.format(itens[computador]))
     print('O Player 1 escolheu {}'.format(itens[jogador]))
+
     if computador == 0:                     # COMPUTADOR JOGOU PEDRA
         if jogador == 0:                    # PEDRA
             print('Empatou!')
@@ -38,18 +49,31 @@ def jokenpo():
         elif jogador == 2:                  # TESOURA
             print('Empatou!')
     else:
-        print('Jogada Invalida!')
+        print('Jogada Inválida!')
     
     again()
 
 def again():
-    executar_novo = input("Quer jogar novamente: [S] Sim ou [N] Não: \n")
-    if executar_novo.upper().strip()[0] == "S" or executar_novo.upper() == 'SIM':
-        jokenpo()   
-        # nome da função principal
-    elif executar_novo.upper().strip()[0] == "N" or executar_novo.upper() == 'NÃO':
+    executar_novo = input("Quer jogar novamente? [S] Sim ou [N] Não: \n")
+    if executar_novo.upper().strip() in ['S', 'SIM']:
+        jokenpo()
+    elif executar_novo.upper().strip() in ['N', 'NÃO']:
         print("Até mais tarde!!!")
     else:
         again()
+        
+def saudacao():
 
+    from datetime import datetime
+   
+    hora_primaria = int(datetime.now().strftime("%H"))
+
+    if hora_primaria < 12:
+        print('Bom dia')
+    elif hora_primaria >= 12 or hora_primaria < 18:
+        print('Boa tarde')
+    elif hora_primaria >= 18 or hora_primaria < 24:
+        print('Boa noite')
+
+    
 jokenpo()
